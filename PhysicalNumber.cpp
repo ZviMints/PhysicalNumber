440c40bb399198ac8b049c175337396167c1bf7e
Zvi Mints from Linux <zvimints@gmail.com>
2019-04-09 22:14:31 +0300

    Final Version

--- PhysicalNumber.cpp
+++ PhysicalNumber.cpp
@@ -257,6 +257,10 @@ int l_index = input.find(']');
 
 if(f_index == -1 || l_index == -1 || f_index >= l_index) 
 {
+    auto errorState = is.rdstate(); // remember error state
+    is.clear(); // clear error so seekg will work
+    is.seekg(startPosition); // rewind
+    is.clear(errorState); // set back the error flag
     return is;
 }
 
