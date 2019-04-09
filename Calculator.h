35ce529066ea9b93774f4dfd7d47c7a4150f2c14
Zvi Mints from Linux <zvimints@gmail.com>
2019-04-09 18:55:09 +0300

    Final Version

--- Calculator.h
+++ Calculator.h
@@ -4,14 +4,14 @@ class Calculator {
     public:
     static double CM_TO_M(double CM) { return 0.01 * CM; }
     static double M_TO_CM(double M) { return 100 * M; }
-    static double M_TO_KM(double M) { return 0.001* M; }
+    static double M_TO_KM(double M) { return M / 1000; }
     static double KM_TO_M(double KM) { return 1000 * KM; }
     static double KM_TO_CM(double KM) { return M_TO_CM(KM_TO_M(KM)); }
     static double CM_TO_KM(double CM) { return M_TO_KM(CM_TO_M(CM)); }
 
     static double G_TO_KG(double G) { return 0.001 * G; }
     static double KG_TO_G(double KG) { return 1000 * KG; }
-    static double KG_TO_TON(double KG) { return 0.001 * KG; }
+    static double KG_TO_TON(double KG) { return KG / 1000 ; }
     static double TON_TO_M(double TON) { return 1000 * TON; }
     static double TON_TO_G(double TON) { return KG_TO_G(TON_TO_M(TON)); }
     static double G_TO_TON(double G) { return KG_TO_TON(G_TO_KG(G)); }
